6c29de20dafff18c6af900d02337036316447b58
FeydOne <dragos.burtescu@gmail.com>
2017-10-16 17:56:58 +0300

    uploading initial config files

---
+++ src/test/java/testsuites/AutomationPractice/scenarios/Homepage.java
@@ -0,0 +1,14 @@
+package testsuites.AutomationPractice.scenarios;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by dburtescu on 10/16/2017.
+ */
+public class Homepage {
+    @Test
+    public void test() throws Throwable {
+
+    }
+}
