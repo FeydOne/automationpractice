dd61576d541505193990038fdb6a35e3b6b24d76
FeydOne <dragos.burtescu@gmail.com>
2017-10-16 18:16:01 +0300

    small config for test cases

--- src/test/java/testsuites/AutomationPractice/scenarios/Homepage.java
+++ src/test/java/testsuites/AutomationPractice/scenarios/Homepage.java
@@ -1,12 +1,18 @@
 package testsuites.AutomationPractice.scenarios;
 
+import com.xceptance.xlt.api.engine.scripting.AbstractWebDriverScriptTestCase;
+
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 /**
  * Created by dburtescu on 10/16/2017.
  */
-public class Homepage {
+public class Homepage extends AbstractWebDriverScriptTestCase {
+    public Homepage(WebDriver driver) {
+        super(driver);
+    }
+
     @Test
     public void test() throws Throwable {
 
